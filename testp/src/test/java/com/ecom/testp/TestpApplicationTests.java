97f489f0709204bfb5070b035f307e054095b286
vikas <vp@vikas.padavala@gmail.com>
2024-06-25 03:39:07 +0000

    creating new test project to test and learn

---
+++ testp/src/test/java/com/ecom/testp/TestpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.testp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
