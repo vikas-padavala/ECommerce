2b0f4c8ba583567db1e7dd7912ec9c13b73c3a51
vikas <vp@vikas.padavala@gmail.com>
2024-06-26 16:14:03 +0000

    added DemoController in testp

--- testp/src/main/java/com/ecom/testp/DemoController.java
+++ testp/src/main/java/com/ecom/testp/DemoController.java
@@ -9,7 +9,7 @@ public class DemoController {
     @GetMapping("/")
     public String helloWorld()
     {
-        return "Hello World !!!, comon baby, lets rock and roll";
+        return "Hello World !!!, fuckin get a new job already!!";
     }
     
 }
